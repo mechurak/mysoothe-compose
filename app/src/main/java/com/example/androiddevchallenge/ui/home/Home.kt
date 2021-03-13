0727f9bcb132499dd26b574a2fc3b15497ed0339
sshim <mechurak@gmail.com>
2021-03-13 19:48:51 +0900

    Prepare HomeScreen stub

--- app/src/main/java/com/example/androiddevchallenge/ui/home/Home.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/home/Home.kt
@@ -1,21 +1,82 @@
 package com.example.androiddevchallenge.ui.home
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import androidx.navigation.compose.navigate
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
 
 @Composable
-fun HomeScreen(navController: NavController? = null) {
+fun HomeScreen(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    navController: NavController? = null
+) {
+    var keyword by remember { mutableStateOf("") }
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {}
+
     Column {
-        Text(text = "Home screen")
-        Button(onClick = { navController!!.navigate("welcome") }) {
-            Text(text = "To Welcome")
-        }
+        TextField(
+            leadingIcon = {
+                Icon(
+                    Icons.Filled.Search,
+                    contentDescription = "search",
+                    modifier = Modifier.size(18.dp)
+                )
+            },
+            placeholder = { Text("Search") },
+            value = keyword,
+            onValueChange = { keyword = it },
+            textStyle = MaterialTheme.typography.body1.copy(color = MaterialTheme.colors.onSurface),
+            colors = TextFieldDefaults.textFieldColors(backgroundColor = MaterialTheme.colors.surface),
+            modifier = Modifier
+                .padding(top = 56.dp, start = 16.dp, end = 16.dp)
+                .height((56).dp)
+                .fillMaxWidth()
+        )
+
+        Text(
+            text = "FAVORITE COLLECTIONS",
+            style = MaterialTheme.typography.h2,
+            color = MaterialTheme.colors.onBackground,
+            modifier = Modifier
+                .paddingFromBaseline(top = 48.dp, bottom = 8.dp)
+                .padding(start = 16.dp)
+        )
+
+        // List
+
+        Text(
+            text = "ALIGN YOUR BODY",
+            style = MaterialTheme.typography.h2,
+            color = MaterialTheme.colors.onBackground,
+            modifier = Modifier
+                .paddingFromBaseline(top = 48.dp, bottom = 8.dp)
+                .padding(start = 16.dp)
+        )
+
+        // List
+
+        Text(
+            text = "ALIGN YOUR MIND",
+            style = MaterialTheme.typography.h2,
+            color = MaterialTheme.colors.onBackground,
+            modifier = Modifier
+                .paddingFromBaseline(top = 48.dp, bottom = 8.dp)
+                .padding(start = 16.dp)
+        )
+
+        // List
     }
 }
 
